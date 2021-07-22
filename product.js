cc1181ca13bd2734d3082174300547c938c39a55
Tim X <80139645+pengbo-XUE@users.noreply.github.com>
2021-07-22 22:29:53 +1000

    sort added

---
+++ product.js
@@ -0,0 +1,16 @@
+function product
+    (
+        P_ID,
+        P_name,
+        P_Price,
+        onSpecial,
+        stock,
+        img
+    ) {
+    this.P_ID = P_ID;
+    this.P_name = P_name;
+    this.P_Price = P_Price;
+    this.onSpecial = onSpecial;
+    this.stock = stock;
+    this.img = img;
+}
---
+++ sort.js
@@ -0,0 +1,71 @@
+
+var p1 = new product(1, 'btest1', 120, false, 10, "img1.jpg");
+var p2 = new product(2, 'atest2', 110, false, 20, "img2.jpg");
+var p3 = new product(3, 'ctest3', 112, false, 13, "img3.jpg");
+var p4 = new product(4, 'dtest4', 140, false, 21, "img4.jpg");
+var productList = [p1, p2, p3, p4];
+
+var content = "";
+
+const productCon = document.querySelectorAll(".product_con");
+const row = document.querySelector("#rowone");
+
+
+
+productList.forEach(displayCardFunc);
+
+
+
+function displayCardFunc(prod) {
+
+    content =
+        ` 
+        <div class="card" style="width: 18rem;">
+            <img src="/images/${prod.img}" class="card-img-top" alt="...">
+            <div class="card-body d-flex flex-column d-flex">
+                <h5 class="card-title">Miss Dior Eau de Parfum celebrates the beauty of a sensual Grasse Rose enveloped in fresh notes and woven with a lively
+                </h5>
+
+                <a class="btn btn-primary mt-auto card-btn">Check it out</a>
+            </div>
+        </div>
+    `;
+    let node = document.createElement("div");
+    node.setAttribute("class", "col-sm  d-flex align-items-stretch");
+    row.appendChild(node);
+    node.innerHTML = content;
+
+}
+
+
+function comparePrice(a, b) {
+
+    if (a.P_Price < b.P_Price) {
+        return -1;
+    }
+    if (a.P_Price > b.P_Price) {
+        return 1;
+    }
+    return 0;
+}
+
+function compareName(a, b) {
+    if (a.P_name < b.P_name) {
+        return -1;
+    }
+    if (a.P_name > b.P_name) {
+        return 1;
+    }
+    return 0;
+}
+
+function sortByPrice() {
+    productList.sort(comparePrice);
+    row.innerHTML = "";
+    productList.forEach(displayCardFunc);
+}
+function sortByName() {
+    productList.sort(compareName);
+    row.innerHTML = "";
+    productList.forEach(displayCardFunc);
+}
