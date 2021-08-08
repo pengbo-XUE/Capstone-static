3e88058cb8815e5b28a0909fbee8e5d85d368168
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2021-08-09 01:02:35 +1000

    Refactor javascipt sort functions.

---
+++ product2.js
@@ -0,0 +1,10 @@
+function product (sku, imgURL, name, gender, brand, type, size, price) {
+  this.sku = sku;
+  this.imgURL = imgURL;
+  this.name = name;
+  this.gender = gender;
+  this.brand = brand;
+  this.type = type;
+  this.size = size;
+  this.price = price;
+}
---
+++ sort2.js
@@ -0,0 +1,48 @@
+var p1 = new product(1, 'img5.jpg', 'Bright Crystal', 'w', 'Versace', "EDT", "30ml", 4500.54);
+var p2 = new product(2, 'img6.jpg', 'Light Blue', 'w', 'Dolce & Gabbana', "EDP", "50ml", 6798.54);
+var p3 = new product(3, 'img3.jpg', 'ctest3', 'w', 'Versace', "EDP", "100ml", 112.00);
+var p4 = new product(4, 'img4.jpg', 'dtest4', 'w', 'Versace', "EDP", "100ml", 140.00);
+var productsData = [p1, p2, p3, p4];
+
+const selectElement = document.querySelector('#product-sorter');
+const productsDisplay = document.querySelector("#products");
+
+selectElement.addEventListener('change', (event) => sortProductsBy(event.target.value));
+
+productsData.forEach(renderProducts);
+
+function renderProducts(prod) {
+  let productHtml = ` 
+			<div>
+				<img src="/images/${prod.imgURL}" alt="${prod.name} - ${prod.brand}">
+				<div>
+					<h4>${prod.name} (${prod.gender})</h4>
+					<p>by ${prod.brand}</p>
+					<p>${prod.type}</p> 
+					<p>${prod.size}</p> 
+					<p>Rs. ${prod.price}</p>
+					<button>
+						Add to cart
+					</button>
+				</div>
+			</div>
+    `;
+    let node = document.createElement("div");
+    node.setAttribute("class", "product");
+    productsDisplay.appendChild(node);
+    node.innerHTML = productHtml;
+}
+
+function sortProductsBy(sortCriteria) {
+	if (sortCriteria === "priceDesc")
+    productsData.sort((a,b) => a.price < b.price ? 1 : -1);
+	if (sortCriteria === "priceAsc")
+    productsData.sort((a,b) => a.price > b.price ? 1 : -1);
+	if (sortCriteria === "nameAsc")
+    productsData.sort((a,b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1);
+	if (sortCriteria === "nameDesc")
+    productsData.sort((a,b) => a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1);
+  
+    productsDisplay.innerHTML = "";
+  productsData.forEach(renderProducts);
+}
